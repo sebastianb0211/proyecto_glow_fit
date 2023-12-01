53f494cc6e58984f8e435607becddefd251fc17c
Sebastianb <sebastianb.0211@gmail.com>
2023-11-30 23:15:46 -0500

    se actualizan botonoes

---
+++ controller/planesPeso.js
@@ -0,0 +1,21 @@
+// botones para mostrar  el contenido de cada plan 
+
+let botonesPlanAdelgazar = document.getElementById("botonesPlanAdelgazar")
+let botonesPlanTonificar = document.getElementById("botonesPlanTonificar")
+let botonesPlanPeso =document.getElementById("botonesPlanPeso")
+
+//botones para mostrar la alimentacion segun el plan
+let botonPlanesAlimenticioAdelgazar = document.getElementById("botonPlanesAlimenticioAdelgazar")
+let botonPlanesAlimenticioTonificar = document.getElementById("botonPlanesAlimenticioTonificar")
+let botonPlanesAlimenticioPeso = document.getElementById("botonPlanesAlimenticioPeso")
+
+// menu para el plan alimenticio segun el plan
+
+let menuAlimentacionAdelgazar = document.getElementById("menuAlimentacionAdelgazar")
+let menuAlimentacionTonificar = document.getElementById("menuAlimentacionTonificar")
+let menuAlimentacionPeso = document.getElementById("menuAlimentacionPeso")
+
+// contenido de plan alimenticio segun el plan
+let contenidoAlimentarioAdelgazar = document.getElementById("contenidoAlimentarioAdelgazar")
+let contenidoAlimentarioTonificar = document.getElementById("contenidoAlimentarioTonificar")
+let contenidoAlimentarioPeso = document.getElementById("contenidoAlimentarioPeso")
